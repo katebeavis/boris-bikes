63c6b3b9203a093527eb3581e75293788f5122f5
Kate Beavis <katebeavis84@gmail.com>
2015-02-05 13:48:51 +0000

    Added default capacity to garage

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,12 +3,16 @@ require 'garage'
 
 describe Garage do
 
-  let(:garage) { Garage.new }
+  let(:garage) { Garage.new(capacity: 1) }
   let(:broken_bike) {double :bike, broken?: true}
   let(:van) {double :van}
 
+  it 'should allow deafult capacity on initializing' do
+    expect(garage.capacity).to eq(1)
+  end
 
-   it 'should fix broken bikes' do
+
+   it 'should dock and fix broken bikes' do
      expect(broken_bike).to receive(:fix!)
      garage.dock(broken_bike)
    end
